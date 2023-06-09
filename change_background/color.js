e69eec82fc22cbabcc0cd3ba04a83a7954c0ba47
Jahmella Williams <simmons.jahmella@gmail.com>
2023-06-08 22:49:31 -0400

    Creates color.js

---
+++ change_background/color.js
@@ -0,0 +1,4 @@
+const values = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
+const btn = document.querySelector("btn");
+const backgroundHex = document.getElementById("background-color");
+const randomNumber = Math.floor(Math.random()*values.length)
