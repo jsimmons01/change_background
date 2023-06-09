98e35567a882c19964d621b06c071da5645d7f1a
Jahmella Williams <simmons.jahmella@gmail.com>
2023-06-08 23:36:59 -0400

    Changes color name

--- change_background/color.js
+++ change_background/color.js
@@ -1,6 +1,6 @@
 const values = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
 const btn = document.querySelector(".btn");
-const backgroundHex = document.getElementById("background-color");
+const backgroundHex = document.getElementById("background-color-name");
 
 
 
@@ -12,6 +12,8 @@ btn.addEventListener('click', function(){
         
     }
 
+    backgroundHex.textContent = color
+    console.log(color)
 });
 
 function getRandomNumber(){
