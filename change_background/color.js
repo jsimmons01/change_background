a04cbc06ac094f30bbe6e066889f4879f3a6886e
Jahmella Williams <simmons.jahmella@gmail.com>
2023-06-08 23:03:06 -0400

    Creates value array

--- change_background/color.js
+++ change_background/color.js
@@ -1,4 +1,9 @@
 const values = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
 const btn = document.querySelector("btn");
 const backgroundHex = document.getElementById("background-color");
-const randomNumber = Math.floor(Math.random()*values.length)
+const randomNumber = Math.floor(Math.random()*values.length);
+const color = '';
+
+btn.addEventListener('click', function(){
+
+})
